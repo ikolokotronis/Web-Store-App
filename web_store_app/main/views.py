3dfa4d676b14d90c11c97249a197b966bc250d1c
ikolokotronis <ioanniskolokotronis1@gmail.com>
2021-12-07 16:12:52 +0100

    Add models reference in views, along with links in html

--- web_store_app/main/views.py
+++ web_store_app/main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category
 # Create your views here.
 
 
 class HomePage(View):
     def get(self, request):
-        return render(request, 'main/base.html')
+        stringed_instruments = Category.objects.get(id=1)
+        keyboard_instruments = Category.objects.get(id=2)
+        drums = Category.objects.get(id=3)
+        sound_system = Category.objects.get(id=4)
+        return render(request, 'main/base.html', {'stringed_instruments': stringed_instruments,
+                                                  'keyboard_instruments': keyboard_instruments,
+                                                  'drums': drums,
+                                                  'sound_system': sound_system}
+                      )
