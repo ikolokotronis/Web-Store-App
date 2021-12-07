d2000c6941fc64d34e831c555864233cce9f14f3
ikolokotronis <ioanniskolokotronis1@gmail.com>
2021-12-07 15:49:14 +0100

    Add models

--- web_store_app/main/models.py
+++ web_store_app/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import User
+from products.models import Product
+
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+
+
+class ShoppingCart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
--- web_store_app/products/models.py
+++ web_store_app/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=500)
+
+    price = models.IntegerField()
+
+    star_choices = (
+        ('1', '*'),
+        ('2', '**'),
+        ('3', '***'),
+        ('4', '****'),
+        ('5', '*****')
+    )
+    rating = models.IntegerField(choices=star_choices)
+
+    is_bestseller = models.BooleanField()
+
+    date_added = models.DateTimeField(auto_now_add=True)
