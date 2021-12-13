1ce48dd0aaacfad6e0e2f48a58739292d557eb66
ikolokotronis <ioanniskolokotronis1@gmail.com>
2021-12-13 01:29:01 +0100

    Update products view and html

--- web_store_app/products/views.py
+++ web_store_app/products/views.py
@@ -9,7 +9,6 @@ from django.http import HttpResponse
 
 class ProductView(View):
     def get(self, request, product_id):
-        last_viewed_product = ""
         stringed_instruments = Category.objects.get(id=1)
         keyboard_instruments = Category.objects.get(id=2)
         drums = Category.objects.get(id=3)
@@ -19,6 +18,7 @@ class ProductView(View):
         product = Product.objects.get(id=product_id)
         shopping_cart = ShoppingCart.objects.all()
         subcategory = SubCategoryProduct.objects.filter(product_id=product_id)[0]
+        last_viewed_products = []
         response = render(request, 'product/product_details.html', {'stringed_instruments': stringed_instruments,
                                                   'keyboard_instruments': keyboard_instruments,
                                                   'drums': drums,
@@ -26,16 +26,16 @@ class ProductView(View):
                                                   'product': product,
                                                                 'shopping_cart_list': shopping_cart,
                                                                 'subcategory': subcategory,
-                                                                'last_viewed_product_id': last_viewed_product,
+                                                                'last_viewed_products': last_viewed_products,
                                                                     'all_categories': all_categories,
                                                                     'all_subcategories': all_subcategories,
                                                                     }
                       )
-        if request.COOKIES.get('last_viewed_product_id'):
-            last_viewed_product = request.COOKIES.get('last_viewed_product_id')
+        if request.COOKIES.get('last_viewed_product'):
+            last_viewed_products.append(request.COOKIES.get('last_viewed_product'))
         else:
-            response.set_cookie(key='last_viewed_product_id', value=product_id, max_age=30)
-            last_viewed_product = request.COOKIES.get('last_viewed_product_id')
+            response.set_cookie(key='last_viewed_product', value=product, max_age=3600)
+            last_viewed_products.append(request.COOKIES.get('last_viewed_product'))
             return response
         return render(request, 'product/product_details.html', {'stringed_instruments': stringed_instruments,
                                                   'keyboard_instruments': keyboard_instruments,
@@ -46,7 +46,7 @@ class ProductView(View):
                                                   'product': product,
                                                                 'shopping_cart_list': shopping_cart,
                                                                 'subcategory': subcategory,
-                                                                'last_viewed_product_id': last_viewed_product}
+                                                                'last_viewed_products': last_viewed_products}
                       )
 
     def post(self, request, product_id):
@@ -54,6 +54,8 @@ class ProductView(View):
         keyboard_instruments = Category.objects.get(id=2)
         drums = Category.objects.get(id=3)
         sound_system = Category.objects.get(id=4)
+        all_categories = Category.objects.all()
+        all_subcategories = SubCategory.objects.all().order_by('name')
         user_id = request.POST.get('user_id')
         quantity = request.POST.get('quantity')
         product = Product.objects.get(id=product_id)
@@ -64,6 +66,8 @@ class ProductView(View):
                                                                 'keyboard_instruments': keyboard_instruments,
                                                                 'drums': drums,
                                                                 'sound_system': sound_system,
+                                                                'all_categories': all_categories,
+                                                                'all_subcategories': all_subcategories,
                                                                 'product': product,
                                                                 'success_text': 'Product added to cart',
                                                                 'shopping_cart_list': shopping_cart}
